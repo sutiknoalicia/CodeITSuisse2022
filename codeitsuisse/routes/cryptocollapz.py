f89be0f3b627d9d2e3034f940391e7bf37d79cdc
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 20:16:50 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -10,6 +10,7 @@ count = {1 : 4, 2 : 4}
 
 
 def hailstone(n):
+	x = n
 	if n not in count:
 		temp = n
 		if n == 1 or n == 2: return 4
@@ -18,7 +19,7 @@ def hailstone(n):
 				temp = n
 			elif n % 2 == 0: n = n // 2
 			else: n = n * 3 + 1
-			count[n] = temp
+		count[x] = temp
 		return temp
 	else: return count[n]
 
