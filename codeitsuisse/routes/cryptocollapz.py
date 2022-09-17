0feaa308f5554aca638a2ad4b3d8e89c1dde4920
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 20:19:27 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -25,6 +25,7 @@ def hailstone(n):
 
 @app.route("/cryptocollapz", methods=['GET', 'POST'])
 def maxPrice():
+	return jsonify([[1, 2, 3, 4], [2, 3, 4, 5]])
 	stream = request.get_json(force=True)
 	
 	arr = pd.DataFrame(stream)
