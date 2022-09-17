eb981dafb17e499cbbbb81801e22822e64645bed
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 22:29:03 +0800

    Update traveling_suisse_robot.py

--- codeitsuisse/routes/traveling_suisse_robot.py
+++ codeitsuisse/routes/traveling_suisse_robot.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request
 from codeitsuisse import app
 import json
 import logging
-
 logger = logging.getLogger(__name__)
 
 class Robot():
@@ -12,11 +11,26 @@ class Robot():
 		for i in range(0, len(mapData)):
 			self.location = (i, i.index("X"))
 			break
+		self.letterLocs = {
+			"c" : []
+			"o" : []
+			"d" : []
+			"e" : []
+			"i" : []
+			"t" : []
+			"s" : []
+			"u" : []
+		}
+
+	def findLetterLocations:
+
 
 @app.route("/traveling-suisse-robot", methods=['GET', 'POST'])
 def main():
 	Map = request.get_data().decode('utf-8').strip()
 	Map = Map.splitlines(True)
 	Map = list(map(rstrip("\n\r\n"), Map))
+
+	bot = Robot(Map)
 
 	return jsonify(Map)
