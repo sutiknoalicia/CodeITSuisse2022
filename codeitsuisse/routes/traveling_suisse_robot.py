16e75eabc69fc8e94c77d24392c03035733bc186
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 22:10:55 +0800

    Update traveling_suisse_robot.py

--- codeitsuisse/routes/traveling_suisse_robot.py
+++ codeitsuisse/routes/traveling_suisse_robot.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, request
 from codeitsuisse import app
 import json
 import logging
+
 logger = logging.getLogger(__name__)
 
-
-@app.route("/traveling-suisse-robot.py", methods=['GET', 'POST'])
+@app.route("/traveling-suisse-robot", methods=['GET', 'POST'])
 def main():
 	Map = request.get_data().decode('utf-8').strip()
 	Map = Map.splitlines(True)
