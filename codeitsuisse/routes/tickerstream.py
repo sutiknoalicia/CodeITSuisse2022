b7851c31c7d3c384fdface46d7e3e1d0339b565c
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 13:13:19 +0800

    Update tickerstream.py

--- codeitsuisse/routes/tickerstream.py
+++ codeitsuisse/routes/tickerstream.py
@@ -83,6 +83,6 @@ def to_cumulative_delayed():
 				counter += 1
 				if (counter) == quantity_block:
 					curr = Timestamps[ticks][i]
-					result.insert(0, str(Ticker(curr.strTime, curr.ticker, counter, cumSum)))
+					result.append(str(Ticker(curr.strTime, curr.ticker, counter, cumSum)))
 		return jsonify({"output" : result})
 		raise Exception
