6c1a6a457148cf8d35707f2711fee6836313f3cf
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 22:19:47 +0800

    Update traveling_suisse_robot.py

--- codeitsuisse/routes/traveling_suisse_robot.py
+++ codeitsuisse/routes/traveling_suisse_robot.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 def main():
 	Map = request.get_data().decode('utf-8').strip()
 	Map = Map.splitlines(True)
-	Map = list(map(rstrip("\n\r\n"), Map))
+	Map = list(map(lambda x: x.rstrip("\n\r\n"), Map))
 	Map = list(map(lambda x: x.split(""), Map))
 
 	return jsonify(Map)
