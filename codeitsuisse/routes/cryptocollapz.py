299ccaa0268592d81c65d281a30cb7a5e24f095c
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 12:29:00 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -16,7 +16,7 @@ def maxPrice():
 			if temp[-1] == 1: result[-1].append(4)
 			elif temp[-1] == 2: result[-1].append(4)
 			else:
-				while temp[-1] != 1:
+				while temp[-1] != 4:
 					if temp[-1] % 2 == 0: temp.append(temp[-1] / 2)
 					else: temp.append(temp[-1] * 3 + 1)
 				result[-1].append(int(max(temp)))
