790863fcfd56eff9e2b0806a2e7c57a3442e7138
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 09:44:57 +0800

    Update tickerstream.py

--- codeitsuisse/routes/tickerstream.py
+++ codeitsuisse/routes/tickerstream.py
@@ -15,28 +15,28 @@ def to_cumulative():
 
 	#finds cumulative_notional for each timestamp (input value)
 	for i in range(len(stream)):
-	#streamL: converts the str into list
-	streamL = stream[i].split(',')
-	timestamp.append(streamL[0])
-	cumulative_notional = str(float(streamL[2])*float(streamL[3]))
-	streamL[3] = cumulative_notional
-	#streamS: converts list into str
-	streamS = ",".join(streamL)
-	streamSL.append(streamS)
+		#streamL: converts the str into list
+		streamL = stream[i].split(',')
+		timestamp.append(streamL[0])
+		cumulative_notional = str(float(streamL[2])*float(streamL[3]))
+		streamL[3] = cumulative_notional
+		#streamS: converts list into str
+		streamS = ",".join(streamL)
+		streamSL.append(streamS)
 
 	#list storing all different timestamps (no repeating)
 	timestamp = sorted(list(set(timestamp)))
 
 	#combines the same tumestamps into 1 string
 	for i in range(len(timestamp)):
-	group = [] #stores the ticker, quantity, notion for a certain timestamp
-	for j in streamSL:
-		if timestamp[i] == j[0:5]:
-		group.append(j[6:])
-	group = sorted(group,key=lambda ticker:ticker[0])
-	#sorts the ticker, quantity,notion by ticker
-	group = ",".join(group)
-	streamFNL.append(timestamp[i]+","+group)
+		group = [] #stores the ticker, quantity, notion for a certain timestamp
+		for j in streamSL:
+			if timestamp[i] == j[0:5]:
+				group.append(j[6:])
+		group = sorted(group,key=lambda ticker:ticker[0])
+		#sorts the ticker, quantity,notion by ticker
+		group = ",".join(group)
+		streamFNL.append(timestamp[i]+","+group)
 	
 	return jsonify({"output" : streamFNL})
 
@@ -51,22 +51,22 @@ def to_cumulative_delayed():
 
 	#identifies the different tickers
 	for i in range(len(stream)):
-	streamL = stream[i].split(',')
-	ticker.append(streamL[1])
+		streamL = stream[i].split(',')
+		ticker.append(streamL[1])
 	ticker = sorted(list(set(ticker)))
 
 	for i in ticker: #will go through the stream for each different ticker
-	cumulative_quantity, cumulative_notional = 0, float(0)
-	for j in range(len(stream)):
-		streamL = stream[j].split(',')
-		if i == streamL[1]:
-		#adds notional and quantity depending on the quantity of the stream
-		for k in range(int(streamL[2])): 
-			cumulative_quantity += 1
-			cumulative_notional += float(streamL[3])
-			if int(cumulative_quantity) == quantity_block:
-			streamFNL.append(streamL[0]+","+i+","+str(cumulative_quantity)+","+str(cumulative_notional))
-			break
+		cumulative_quantity, cumulative_notional = 0, float(0)
+		for j in range(len(stream)):
+			streamL = stream[j].split(',')
+			if i == streamL[1]:
+				#adds notional and quantity depending on the quantity of the stream
+				for k in range(int(streamL[2])): 
+					cumulative_quantity += 1
+					cumulative_notional += float(streamL[3])
+					if int(cumulative_quantity) == quantity_block:
+						streamFNL.append(streamL[0]+","+i+","+str(cumulative_quantity)+","+str(cumulative_notional))
+						break
 	
 	streamFNL = sorted(streamFNL)
-	return jsonify({"output" : streamFNL}) #returns sorted stream where timestamps are in chronological order
+	return jsonify({"output" : result}) #returns sorted stream where timestamps are in chronological order
