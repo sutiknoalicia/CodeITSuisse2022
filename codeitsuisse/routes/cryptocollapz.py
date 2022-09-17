705ee4b3900eecd2ad9fff8e66a8022daa879bb3
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 15:22:01 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -21,11 +21,11 @@ def maxPrice():
 	for price in np.nditer(arr, op_flags=['readwrite'], flags=["refs_ok"]):
 		if not price == 1 or not price == 2:
 			temp = price
-			while price != 1:
+			while price[...] != 1:
 				if price > temp:
 					temp = price
-				price = hailstone(price)
-				price = temp
-		else: price = 4
+				price[...] = hailstone(price[...])
+				price[...] = temp
+		else: price[...] = 4
 	
 	return jsonify(arr.tolist())
