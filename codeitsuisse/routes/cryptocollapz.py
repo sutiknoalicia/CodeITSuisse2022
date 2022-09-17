e20045dafd3d1cc4096b11b22ac32c6dd91d7754
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 20:07:21 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -16,11 +16,14 @@ def hailstone(n):
 		while n != 1:
 			if n > temp:
 				temp = n
-			if n % 2 == 0: n = n // 2
+			if n in count:
+				n = count[n]
+			elif n % 2 == 0: n = n // 2
 			else: n = n * 3 + 1
 		count[n] = temp
 		return temp
 	else: return count[n]
+
 @app.route("/cryptocollapz", methods=['GET', 'POST'])
 def maxPrice():
 	stream = request.get_json(force=True)
