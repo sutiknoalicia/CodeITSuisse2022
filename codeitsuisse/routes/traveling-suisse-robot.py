21be94ce5e801d57e3ab12dd6a7a10e1e38d8816
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 20:58:19 +0800

    Create traveling-suisse-robot.py

---
+++ codeitsuisse/routes/traveling-suisse-robot.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from codeitsuisse import app
+import json
+import logging
+logger = logging.getLogger(__name__)
+
+def strToMap(strMap):
+	Map = [line.split(" ") for line in strMap.split("\n")]
+
+
+@app.route("/travelling-suisse-robot", methods=['GET', 'POST'])
+def main():
+	Map = request.get_json(force=True)
+	return Map
