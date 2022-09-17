96106daa431be0c1682a9fefc9e1df0894040c37
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 15:11:03 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -16,7 +16,7 @@ def hailstone(n):
 def maxPrice():
 	stream = request.get_json(force=True)
 	
-	arr = np.ndarray(stream, dtype=int)
+	arr = np.asarray(stream)
 	
 	for price in np.nditer(arr, op_flags=['readwrite']):
 		if not price == 1 or not price == 2:
