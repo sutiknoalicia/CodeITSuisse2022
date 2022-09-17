07aae0b18fe97ba1aaa667fb61516d9dd3db6f79
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 10:19:11 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -6,15 +6,18 @@ import logging
 logger = logging.getLogger(__name__)
 
 @app.route("/cryptocollapz", methods=['GET', 'POST'])
-def maxPrice():
+def maxPrice(array):
 	stream = json.loads(request.data)
 	result = []
 	for prices in stream:
 		result.append([])
 		for price in prices:
 			temp = [int(price)]
-			while temp[-1] != 1:
-				if temp[-1] % 2 == 0: temp.append(temp[-1] / 2)
-				else: temp.append(temp[-1] * 3 + 1)
-			result[-1].append(max(temp))
+			if temp[-1] == 1: result[-1].append(4)
+			elif temp[-1] == 2: result[-1].append(4)
+			else:
+				while temp[-1] != 1:
+					if temp[-1] % 2 == 0: temp.append(temp[-1] / 2)
+					else: temp.append(temp[-1] * 3 + 1)
+				result[-1].append(int(max(temp)))
 	return jsonify("data" : result)
