ed02962e5ed549ead52e4c0a4bcebf64567fad67
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 22:04:10 +0800

    Update traveling_suisse_robot.py

--- codeitsuisse/routes/traveling_suisse_robot.py
+++ codeitsuisse/routes/traveling_suisse_robot.py
@@ -4,10 +4,11 @@ import json
 import logging
 logger = logging.getLogger(__name__)
 
+
 @app.route("/travelling-suisse-robot", methods=['GET', 'POST'])
-@accept('text/plain')
 def main():
 	Map = request.get_data().decode('utf-8').strip()
 	Map = Map.splitlines(True)
 
 	return jsonify(Map)
+
