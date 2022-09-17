b3db1a1c3d26ab5c4f0140e9f21ff3e0968869d9
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 14:30:39 +0800

    Update cryptocollapz.py

--- codeitsuisse/routes/cryptocollapz.py
+++ codeitsuisse/routes/cryptocollapz.py
@@ -5,17 +5,23 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+def hailstone(n):
+    if n % 2 == 0:
+        return n // 2
+    else:
+        return 3 * n + 1
+
 @app.route("/cryptocollapz", methods=['GET', 'POST'])
 def maxPrice():
 	stream = request.get_json(force=True)
 	for key1, prices in enumerate(stream):
 		for key2, price in enumerate(prices):
 			if not price == 1 or not price == 2:
-				temp = [price]
-				while temp[-1] != 2:
-					if temp[-1] % 2 == 0: temp.append(temp[-1] / 2)
-					else: temp.append(temp[-1] * 3 + 1 / 2)
-				stream[key1][key2] = int(max(temp))
+				temp = price
+				while price != 2:
+					if price > temp:
+						temp = price
+					price = hailstone(price)
 			else:
 				stream[key1][key2] = 4
 	return jsonify(stream)
