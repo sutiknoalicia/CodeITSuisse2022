1122f76850f330a0c0195a15285f2bcff411a77b
Davwed0 <98845656+Davwed0@users.noreply.github.com>
2022-09-17 22:57:16 +0800

    Update traveling_suisse_robot.py

--- codeitsuisse/routes/traveling_suisse_robot.py
+++ codeitsuisse/routes/traveling_suisse_robot.py
@@ -4,12 +4,17 @@ import json
 import logging
 logger = logging.getLogger(__name__)
 
+def EuclieanDistance(initial, target):
+    x1, y1 = initial
+    x2, y2 = target
+    return ((y2-y1)**2+(x2-x1)**2)**0.5
+
 class Robot():
 	def __init__(self, mapData):
 		self.map = mapData
 		self.location = None
 		for i in range(0, len(mapData)):
-			self.location = (i, mapData[i].find("X"))
+			self.location = (i, mapData[i].index("X"))
 			break
 		self.letterLocs = {
 			"c" : [],
@@ -21,6 +26,7 @@ class Robot():
 			"s" : [],
 			"u" : [],
 		}
+		self.string = "codeitsuisse"
 
 	def findLetterLocations(self):
 		number = 12
@@ -31,6 +37,15 @@ class Robot():
 					number -= 1
 				else: break
 
+	def findNext(self, target):
+		target = self.string[target]
+		distances = []
+		for i in range(0, len(letterLocs[target])):
+			distances.append(EuclieanDistance(self.location, letterLocs[target][i]))
+		distance, idx = min((distance, idx) for (idx, distance) in enumerate(distances))
+		return letterLocs[target][idx]
+
+
 @app.route("/traveling-suisse-robot", methods=['GET', 'POST'])
 def main():
 	Map = request.get_data().decode('utf-8').strip()
@@ -38,5 +53,9 @@ def main():
 	Map = list(map(lambda x: x.rstrip("\n\r\n"), Map))
 
 	bot = Robot(Map)
+
+	bot.findLetterLocations()
+
+	for i in range(0, )
 
 	return jsonify(Map) 
